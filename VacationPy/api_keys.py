25d24b51f95bc07846d59b537f8aff003faeefac
John Loucks <johnloucks@gmail.com>
2020-09-15 09:29:11 -0500

    updats to vacationpy

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "057058d4186c59a0e2e3953fafd752da"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDXFQ4iewa2QN1sn8anKxMMCPhEmMCTLWk"
